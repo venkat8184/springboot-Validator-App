3c08dc9df53ddcf62d9c67708ca5effa01937566
venkat8184 <venkatramana8184@gmail.com>
2023-03-30 11:40:35 +0530

    committing new changes for feature-301

--- src/main/java/com/validator/ValidatorApplication.java
+++ src/main/java/com/validator/ValidatorApplication.java
@@ -1,11 +1,11 @@
 package com.validator;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
 @SpringBootApplication
 public class ValidatorApplication {
-
+HashMap<Integer,String> hash=new HashMap<Integer,String>();
 	public static void main(String[] args) {
 		SpringApplication.run(ValidatorApplication.class, args);
 	}
